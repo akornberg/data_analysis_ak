d58610e33c0f4bb7176cee466b14147cf379c7ce
akornberg <akornberg5@gmail.com>
2020-04-14 11:23:18 -0400

    First day. Seeing what we need to complete this script.

---
+++ tcr_sharing_clusters.R
@@ -0,0 +1,7 @@
+#Expected TCR Sharing Between Clusters - Adam Kornberg 
+
+#What I need to have
+
+## How many TCRs appear in a given cluster
+## Amount of times a TCR appears in each cluster (of a given patient/mouse)
+## Total # of Clusters
